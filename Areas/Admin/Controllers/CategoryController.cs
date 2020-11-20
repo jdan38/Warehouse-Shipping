6461d37fdea847125da882bd697b0ea012768bbf
jdan38 <jdev0016@gmail.com>
2020-11-20 00:26:39 -0500

    Add project files.

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Warehouse.Data;
+
+namespace Warehouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Get 
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _db.Category.ToListAsync());
+        }
+    }
+}
---
+++ Models/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Models
+{
+    public class Inventory
+    {
+        [Key]
+        public int IvenId { get; set; }
+        public string ItemName { get; set; }
+
+        public int Qyt { get; set; }
+
+        public int Price { get; set; }
+    }
+}
